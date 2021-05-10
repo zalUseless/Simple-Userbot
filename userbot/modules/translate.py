4956f4e07dab079119ba155d732661d2d45a6b8c
Zal <50516841+rizgustiadi@users.noreply.github.com>
2021-05-10 10:10:13 +0700

    Create translate.py

---
+++ userbot/modules/translate.py
@@ -0,0 +1,57 @@
+"""
+Google Translate
+Available Commands:
+tr LanguageCode as reply to a message
+tr LangaugeCode | text to translate
+"""
+
+import emoji
+from googletrans import Translator
+
+from userbot import CMD_HELP
+from userbot.events import register
+
+
+
+@register(outgoing=True, pattern='^tr(?: |$)(.*)')
+async def _(event):
+    if event.fwd_from:
+        return
+    if "trim" in event.raw_text:
+        return
+    x = await eor(event, "Translating...")
+    input_str = event.pattern_match.group(1)
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        text = previous_message.message
+        lan = input_str or "ml"
+    elif "|" in input_str:
+        lan, text = input_str.split("|")
+    else:
+        await x.edit(
+            f"`tr LanguageCode` as reply to a message.\nLanguage codes can be found [here](https://t.me/TeleBotHelpChat/22678)",
+        )
+        return
+    text = emoji.demojize(text.strip())
+    lan = lan.strip()
+    translator = Translator()
+    try:
+        translated = translator.translate(text, dest=lan)
+        after_tr_text = translated.text
+        output_str = """
+**Tʀᴀɴsʟᴀᴛɪᴏɴ**
+**{} ➟ {}**
+`{}`""".format(
+            translated.src, lan, after_tr_text
+        )
+        await x.edit(output_str)
+    except Exception as exc:
+        await x.edit(f"Error\n `{str(exc)}`")
+
+
+CMD_HELP.update(
+    {
+        "translate": "tr <LanguageCode> <reply to a message>\
+        \nUsage - Translate the replied text."
+    }
+)
