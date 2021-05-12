99c859465730938a877a2d6dfedd60ecd6d7a346
Zal <50516841+rizgustiadi@users.noreply.github.com>
2021-05-12 10:36:55 +0700

    Update FastTelethon.py

--- userbot/utils/FastTelethon.py
+++ userbot/utils/FastTelethon.py
@@ -1,3 +1,6 @@
+# kanged from OUBnew
+
+
 """
 > Based on parallel_file_transfer.py from mautrix-telegram, with permission to distribute under the MIT license
 > Copyright (C) 2019 Tulir Asokan - https://github.com/tulir/mautrix-telegram
