40c48b5482e7da10e232eea6c71c61f0f7fb383a
Zal <50516841+rizgustiadi@users.noreply.github.com>
2021-05-14 19:05:40 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -248,7 +248,7 @@ def paginate_help(page_number, loaded_modules, prefix):
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
     helpable_modules = sorted(helpable_modules)
     modules = [
-        custom.Button.inline("{} {}🛠".format("🛠", x), data="ub_modul_{}".format(x))
+        custom.Button.inline("{} {} {}".format("🛠", x), data="ub_modul_{}".format(x))
         for x in helpable_modules
     ]
     pairs = list(zip(modules[::number_of_cols],
@@ -266,7 +266,8 @@ def paginate_help(page_number, loaded_modules, prefix):
                 custom.Button.inline(
                     "║ ⌫️ ║", data="{}_prev({})".format(prefix, modulo_page)
                 ),
-                custom.Button.inline("║ Close ║", data="close"),
+                custom.Button.inline(
+                    "║Close║", data="{}_close({})".format(prefix, modulo_page)
                 custom.Button.inline(
                     "║ ⌦️ ║", data="{}_next({})".format(prefix, modulo_page)
                 )
@@ -371,15 +372,20 @@ with bot:
 
         @tgbot.on(
             events.callbackquery.CallbackQuery( # pylint:disable=E0602
-                data=re.compile(rb"close\((.+?)\)")
+                data=re.compile(rb"help_close\((.+?)\)")
             )
         )
         async def on_plug_in_callback_query_handler(event):
             if event.query.user_id == uid: # pylint:disable=E0602
+                current_page_number = int(
+                    event.data_match.group(1).decode("UTF-8"))
+                buttons = paginate_help(
+                    current_page_number - 1, dugmeler, "help"  # pylint:disable=E0602
+                )
+                # https://t.me/TelethonChat/115200
                 await event.edit(
-                    "Menu Closed!!", buttons=[Button.inline("Re-open Menu", data="reopen")]
+                    "Menu Closed!!", buttons=buttons)
             )
-
             else:
                 reply_pop_up_alert = "Ini Bot Gua Jancokk..  "
                 await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
