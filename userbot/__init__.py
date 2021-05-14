175e7263666e3e3d9ab8af6e90ae52004465a995
Zal <50516841+rizgustiadi@users.noreply.github.com>
2021-05-14 19:10:46 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -386,7 +386,7 @@ with bot:
                 # https://t.me/TelethonChat/115200
                 await event.edit(
                     "Menu Closed!!", buttons=buttons)
-            )
+                )
             else:
                 reply_pop_up_alert = "Ini Bot Gua Jancokk..  "
                 await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
