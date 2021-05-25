d0861306716d51f1298bfc88f662a2bb19d2bcfa
Zal <50516841+rizgustiadi@users.noreply.github.com>
2021-05-25 09:24:22 +0700

    Update broadcast.py

--- userbot/modules/broadcast.py
+++ userbot/modules/broadcast.py
@@ -5,8 +5,8 @@
 from userbot.events import register
 from userbot import CMD_HELP, bot
 
-@register(outgoing=True, pattern="^.sending (.*)")
-async def sending(event):
+@register(outgoing=True, pattern="^.send (.*)")
+async def bcast(event):
     a = event.pattern_match.group(1)
     if not a:
         return await event.edit("`Please Give A Message..`")
@@ -23,6 +23,7 @@ async def sending(event):
                 await bot.send_message(chat, msg)
             except BaseException:
                 d += 1
-    await c.edit(f"**Successfully Sending Message To** `{done}` **Group, Failed To Send Message To** `{er}` **Group**")
+    await c.edit(f"**Successfully Sending Message To** `{done}` **Group, Failed To Send Message To** `{d}` **Group**")
 
-CMD_HELP.update({"broadcast": "`.sending <pesan>`\\nPenjelasan: Global Broadcast mengirim pesan ke Seluruh Grup yang Lord Masuki."})
+CMD_HELP.update({"broadcast": "`.sending <pesan>`\
+                \nPenjelasan: Global Broadcast mengirim pesan ke Seluruh Grup yang Lord Masuki."})
