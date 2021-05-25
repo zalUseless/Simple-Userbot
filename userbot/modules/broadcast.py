30314853f34444fedbf48d87cd14c448d4cc0832
Zal <50516841+rizgustiadi@users.noreply.github.com>
2021-05-25 09:16:52 +0700

    Create broadcast.py

---
+++ userbot/modules/broadcast.py
@@ -0,0 +1,28 @@
+# credits ultroid
+# lorduserbot
+
+
+from userbot.events import register
+from userbot import CMD_HELP, bot
+
+@register(outgoing=True, pattern="^.sending (.*)")
+async def sending(event):
+    a = event.pattern_match.group(1)
+    if not a:
+        return await event.edit("`Please Give A Message..`")
+    b = event.text
+    msg = b[6:]
+    c = await event.edit("`Wait Is Sending Messages Globally....`")
+    d = 0
+    done = 0
+    async for e in bot.iter_dialogs():
+        if e.is_group:
+            chat = e.id
+            try:
+                done += 1
+                await bot.send_message(chat, msg)
+            except BaseException:
+                d += 1
+    await c.edit(f"**Successfully Sending Message To** `{done}` **Group, Failed To Send Message To** `{er}` **Group**")
+
+CMD_HELP.update({"broadcast": "`.sending <pesan>`\\nPenjelasan: Global Broadcast mengirim pesan ke Seluruh Grup yang Lord Masuki."})
