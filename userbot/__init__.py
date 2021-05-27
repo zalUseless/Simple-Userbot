52187960155c4d62d9adf564b1da281605d7106d
Zal <50516841+rizgustiadi@users.noreply.github.com>
2021-05-27 08:46:12 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -249,7 +249,7 @@ def paginate_help(page_number, loaded_modules, prefix):
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
     helpable_modules = sorted(helpable_modules)
     modules = [
-        custom.Button.inline("{} {}🛠".format("🛠", x), data="ub_modul_{}".format(x))
+        custom.Button.inline("{} {}🐊".format("🐊", x), data="ub_modul_{}".format(x))
         for x in helpable_modules
     ]
     pairs = list(zip(modules[::number_of_cols],
@@ -265,11 +265,11 @@ def paginate_help(page_number, loaded_modules, prefix):
         ] + [
             (
                 custom.Button.inline(
-                    "║ ⌫️ ║", data="{}_prev({})".format(prefix, modulo_page)
+                    "║ ⌫️ Prev ║", data="{}_prev({})".format(prefix, modulo_page)
                 ),
                 custom.Button.inline("║ Close ║", data="close"),
                 custom.Button.inline(
-                    "║ ⌦️ ║", data="{}_next({})".format(prefix, modulo_page)
+                    "║ ⌦️ Next ║", data="{}_next({})".format(prefix, modulo_page)
                 )
             )
         ]
