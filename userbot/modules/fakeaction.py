0fb9d7e4e1505c7f4a438ba4ab2cbc23d52aed88
Zal <50516841+rizgustiadi@users.noreply.github.com>
2021-06-02 07:46:21 +0700

    Update fakeaction.py
    Anj

--- userbot/modules/fakeaction.py
+++ userbot/modules/fakeaction.py
@@ -12,8 +12,8 @@ from userbot import CMD_HELP
 from userbot.events import register
 
 @register(outgoing=True, pattern="^.ftyping (.*)")
-async def _(event):
-    l = event.pattern_match.group(1)
+async def _(anjink):
+    l = anjink.pattern_match.group(1)
     if not (l or l.isdigit()):
         l = 100
     else:
@@ -21,17 +21,17 @@ async def _(event):
             l = int(l)
         except BaseException:
             try:
-                l = await event.ban_time(l)
+                l = await anjink.ban_time(l)
             except BaseException:
-                return await event.edit( "`Incorrect Format`")
-    await event.edit(f"Starting Fake Typing For {l} sec.")
-    async with event.client.action(event.chat_id, "typing"):
+                return await anjink.edit( "`Incorrect Format`")
+    await anjink.edit(f"Starting Fake Typing For {l} sec.")
+    async with anjink.client.action(anjink.chat_id, "typing"):
         await asyncio.sleep(l)
 
 
 @register(outgoing=True, pattern="^.faudio (.*)")
-async def _(event):
-    l = event.pattern_match.group(1)
+async def _(anjink):
+    l = anjink.pattern_match.group(1)
     if not (l or l.isdigit()):
         l = 100
     else:
@@ -39,17 +39,17 @@ async def _(event):
             l = int(l)
         except BaseException:
             try:
-                l = await event.ban_time(l)
+                l = await anjink.ban_time(l)
             except BaseException:
-                return await event.edit("`Incorrect Format`")
-    await event.edit(f"Starting Fake audio recording For {l} sec.")
-    async with event.client.action(event.chat_id, "record-audio"):
+                return await anjink.edit("`Incorrect Format`")
+    await anjink.edit(f"Starting Fake audio recording For {l} sec.")
+    async with anjink.client.action(anjink.chat_id, "record-audio"):
         await asyncio.sleep(l)
 
 
 @register(outgoing=True, pattern="^.fvideo (.*)")
-async def _(event):
-    l = event.pattern_match.group(1)
+async def _(anjink):
+    l = anjink.pattern_match.group(1)
     if not (l or l.isdigit()):
         l = 100
     else:
@@ -57,17 +57,17 @@ async def _(event):
             l = int(l)
         except BaseException:
             try:
-                l = await event.ban_time(l)
+                l = await anjink.ban_time(l)
             except BaseException:
-                return await event.edit("`Incorrect Format`")
-    await event.edit(f"Starting Fake video recording For {l} sec.")
-    async with event.client.action(event.chat_id, "record-video"):
+                return await anjink.edit("`Incorrect Format`")
+    await anjink.edit(f"Starting Fake video recording For {l} sec.")
+    async with anjink.client.action(anjink.chat_id, "record-video"):
         await asyncio.sleep(l)
 
 
 @register(outgoing=True, pattern="^.fgame (.*)")
-async def _(event):
-    l = event.pattern_match.group(1)
+async def _(anjink):
+    l = anjink.pattern_match.group(1)
     if not (l or l.isdigit()):
         l = 100
     else:
@@ -75,11 +75,11 @@ async def _(event):
             l = int(l)
         except BaseException:
             try:
-                l = await event.ban_time(l)
+                l = await anjink.ban_time(l)
             except BaseException:
-                return await event.edit("`Incorrect Format`")
-    await event.edit(f"Starting Fake Game Playing For {l} sec.")
-    async with event.client.action(event.chat_id, "game"):
+                return await anjink.edit("`Incorrect Format`")
+    await anjink.edit(f"Starting Fake Game Playing For {l} sec.")
+    async with anjink.client.action(anjink.chat_id, "game"):
         await asyncio.sleep(l)
 
 CMD_HELP.update(
