ca482efd30b7fff6b90156be783f0e702a12dfa5
Zal <50516841+rizgustiadi@users.noreply.github.com>
2021-06-29 19:13:31 +0700

    Create lirik.py

---
+++ userbot/modules/lirik.py
@@ -0,0 +1,27 @@
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from userbot import bot, CMD_HELP
+from userbot.events import register
+
+@register(outgoing=True, pattern=r"^\.lirik(?: |$)(.*)")
+async def _(goblok):
+    if goblok.fwd_from:
+        return
+    chat = "@iLyricsBot"  # pylint:disable=E0602
+    lirik = f"start"  # pylint:disable=E0602
+    await goblok.edit("Tunggu Bro... ")
+    async with bot.conversation("@iLyricsBot") as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(
+                    incoming=True,
+                    from_users=232268607))
+            await conv.send_message(f'/{lirik}')
+            response = await response
+        except YouBlockedUserError:
+            await goblok.reply("Unblock @iLyricsBot dulu Goblok!!")
+            return
+        else:
+            await goblok.edit(f"{response.message.message}")
+            await goblok.client.delete_messages(response.message.message)
+CMD_HELP.update({"lirik": "`.lirik <judul - penyanyi>`" "\nUsage: Untuk Memcari Lirik Lagu."})
