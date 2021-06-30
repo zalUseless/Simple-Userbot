2a01832fd5c68aebf4d8e0653d2731b494d46de1
Zal <50516841+rizgustiadi@users.noreply.github.com>
2021-06-30 13:01:03 +0700

    Update truth\dare.py

--- userbot/modules/truth\dare.py
+++ userbot/modules/truth\dare.py
@@ -24,4 +24,40 @@ async def _(kontol):
         else:
             await kontol.edit(f"{response.message.message}")
             await kontol.client.delete_messages(response.message.message)
-CMD_HELP.update({"truth\dare": "`.dare`" "\nUsage: tantangan."})
+
+
+@register(outgoing=True, pattern=r"^\.truth(?: |$)(.*)")
+async def _(kontol):
+    if kontol.fwd_from:
+        return
+    chat = "@truthordares_bot"  # pylint:disable=E0602
+    tolol = f"truth"  # pylint:disable=E0602
+    await kontol.edit("Tunggu Bro... ")
+    async with bot.conversation("@truthordares_bot") as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(
+                    incoming=True,
+                    from_users=1335899453))
+            await conv.send_message(f'/{tolol}')
+            response = await response
+        except YouBlockedUserError:
+            await kontol.reply("Unblock @truthordares_bot dulu Goblok!!")
+            return
+        else:
+            await kontol.edit(f"{response.message.message}")
+            await kontol.client.delete_messages(response.message.message
+
+
+
+
+
+
+
+CMD_HELP.update(
+    {"truth\dare": "`.dare`\
+    \nUsage: tantangan.\
+    \n\n `truth`\
+    \nUsage: kejujuran."
+ }
+)
